1213c377ac1d62bcf5a6204f26ed7d2721b2ca5c
Gauri0227 <90912721+Gauri0227@users.noreply.github.com>
2021-09-17 23:57:24 +0530

    Update fp2.2.py

--- fp2.2.py
+++ fp2.2.py
@@ -42,8 +42,8 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.metrics.pairwise import euclidean_distances
 from nltk import FreqDist
-from matplotlib.pyplot import figure
-import pickle
+#from matplotlib.pyplot import figure
+#import pickle
 import pandas as pd
 import numpy as np
 
